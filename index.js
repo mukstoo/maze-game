a5e92b2854e8be0f11476b50eb4337463f5210bd
mukstoo <rodrigogreconunes@gmail.com>
2022-07-22 12:40:05 -0300

    maze-generation complete

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
-const { Engine, Render, Runner, World, Bodies, MouseConstraint, Mouse } = Matter;
+const { Engine, Render, Runner, World, Bodies } = Matter;
 
-const width = 800;
+const cells = 10;
+const width = 600;
 const height = 600;
+const unitLenght = width / cells
 
 const engine = Engine.create();
 const { world } = engine;
@@ -16,31 +18,100 @@ const render = Render.create({
 })
 Render.run(render)
 Runner.run(Runner.create(), engine)
-World.add(world, MouseConstraint.create(engine, {
-    mouse: Mouse.create(render.canvas)
-}))
+
 
 // Walls
 const walls = [
-    Bodies.rectangle(400, 0, 800, 40, { isStatic: true }),
-    Bodies.rectangle(400, 600, 800, 40, { isStatic: true }),
-    Bodies.rectangle(0, 300, 40, 600, { isStatic: true }),
-    Bodies.rectangle(800, 300, 40, 600, { isStatic: true })
+    Bodies.rectangle(width / 2, 0, width, 40, { isStatic: true }),
+    Bodies.rectangle(width / 2, height, width, 40, { isStatic: true }),
+    Bodies.rectangle(0, height / 2, 40, height, { isStatic: true }),
+    Bodies.rectangle(width, height / 2, 40, height, { isStatic: true })
 ]
 World.add(world, walls)
 
-//Random shapes
-for (let i = 0; i < 20; i++) {
-    if(Math.random() > 0.5) {
-        World.add(world, Bodies.rectangle(Math.random()*width, Math.random()*height, 50, 50))
-    } else {
-        World.add(world, Bodies.circle(Math.random()*width, Math.random()*height, 35, {
-            render: {
-                fillStyle: 'green'
-            }
-        }))
+//maze generation
+
+const shuffle = (arr) => {
+    let counter = arr.length
+    while (counter > 0) {
+        const index = Math.floor(Math.random() * counter)
+        counter--
+        const temp = arr[counter]
+        arr[counter] = arr[index]
+        arr[index] = temp
     }
-    
+    return arr
 }
 
-    
+const grid = Array(cells).fill(null).map(() => Array(cells).fill(false))
+const verticals = Array(cells).fill(null).map(() => Array(cells - 1).fill(false))
+const horizontals = Array(cells - 1).fill(null).map(() => Array(cells).fill(false))
+
+const startRow = Math.floor(Math.random() * cells)
+const startColumn = Math.floor(Math.random() * cells)
+const stepThroughCell = (row, column) => {
+    if (grid[row][column]) {
+        return
+    }
+    grid[row][column] = true;
+    const neighbors = shuffle([
+        [row - 1, column, 'up'],
+        [row, column + 1, 'right'],
+        [row + 1, column, 'down'],
+        [row, column - 1, 'left'],
+    ])
+    for (let neighbor of neighbors) {
+        const [nextRow, nextColumn, direction] = neighbor
+        if (nextRow < 0 || nextRow >= cells || nextColumn < 0 || nextColumn >= cells) {
+            continue
+        }
+        if (grid[nextRow][nextColumn]) {
+            continue
+        }
+        if (direction === 'left') {
+            verticals[row][column - 1] = true
+        } else if (direction === 'right') {
+            verticals[row][column] = true
+        } else if (direction === 'up') {
+            horizontals[row - 1][column] = true
+        } else if (direction === 'down') {
+            horizontals[row][column] = true
+        }
+
+        stepThroughCell(nextRow, nextColumn)
+    }
+}
+
+stepThroughCell(startRow, startColumn);
+
+horizontals.forEach((row, rowIndex) => {
+    row.forEach((open, columnIndex) => {
+        if (open) {
+            return
+        }
+        const wall = Bodies.rectangle(
+            columnIndex * unitLenght + unitLenght / 2,
+            rowIndex * unitLenght + unitLenght,
+            unitLenght,
+            10,
+            { isStatic: true }
+        )
+        World.add(world, wall)
+    })
+})
+
+verticals.forEach((row, rowIndex) => {
+    row.forEach((open, columnIndex) => {
+        if (open) {
+            return
+        }
+        const wall = Bodies.rectangle(            
+            columnIndex * unitLenght + unitLenght,
+            rowIndex * unitLenght + unitLenght / 2,
+            10,
+            unitLenght,            
+            { isStatic: true }
+        )
+        World.add(world, wall)
+    })
+})
